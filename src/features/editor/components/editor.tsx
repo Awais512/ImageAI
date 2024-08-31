12c83034c656b5712dc6e9adbca94fe0353d6ca6
awais512 <raza.awais25@gmail.com>
2024-08-31 20:38:45 +0100

    Setting up fabric.js

---
+++ src/features/editor/components/editor.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEditor } from "@/features/hooks/use-editor";
+import { useEffect, useRef } from "react";
+import { fabric } from "fabric";
+
+export const Editor = () => {
+  const { init } = useEditor();
+
+  const canvasRef = useRef(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const canvas = new fabric.Canvas(canvasRef.current, {
+      controlsAboveOverlay: true,
+      preserveObjectStacking: true,
+    });
+
+    init({
+      initialCanvas: canvas,
+      initialContainer: containerRef.current!,
+    });
+  }, [init]);
+
+  return (
+    <div className="h-full flex flex-col">
+      <div className="flex-1 h-full bg-muted" ref={containerRef}>
+        <canvas ref={canvasRef} />
+      </div>
+    </div>
+  );
+};
