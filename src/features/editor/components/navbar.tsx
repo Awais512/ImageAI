8fc7077d8f42d7f0de5f6f5589f8ddc40906eec5
awais512 <raza.awais25@gmail.com>
2024-09-01 08:47:55 +0100

    Editor initial  layout

---
+++ src/features/editor/components/navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div className="w-full flex items-center p-4 h-[68px] gap-x-8 border-b lg:pl-[34px]">
+      Navbar
+    </div>
+  );
+};
